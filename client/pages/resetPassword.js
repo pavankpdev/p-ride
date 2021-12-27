2cb8e501b3faa578de23519481a163e231a3615a
Bharath M U <bharathmogaveer01@gmail.com>
2021-12-27 22:11:22 +0530

    validate-pages

--- client/pages/resetPassword.js
+++ client/pages/resetPassword.js
@@ -1,86 +1,89 @@
 import {
-    Button,
-    Flex,
-    FormControl,
-    FormLabel,
-    Heading,
-    Input,
-    Stack,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  import { useState } from 'react';
-  
-  const ResetPassword = () => {
-      const [resetInput, setResetInput] = useState({ 
-            newPassword: "",
-            confirmPassword: "",
-      });
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-      const handleChange = (event) => {
-        setResetInput({ ...resetInput, [event.target.name]: event.target.value})
-      };
+const ResetPassword = () => {
+  const [resetInput, setResetInput] = useState({
+    newPassword: "",
+    confirmPassword: "",
+  });
 
-      const handleSubmit = () => {
-          console.log(resetInput);
-          if(resetInput.newPassword !== resetInput.confirmPassword){
-            console.log("Password not equal");
-            return;
-          }
-          console.log(resetInput);
-      }
+  const handleChange = (event) => {
+    setResetInput({ ...resetInput, [event.target.name]: event.target.value });
+  };
 
+  const handleSubmit = () => {
+    console.log(resetInput);
+    if (resetInput.newPassword !== resetInput.confirmPassword) {
+      alert("Password not equal");
+      return;
+    }
+    if (!resetInput.newPassword) {
+      alert("Password cannot be empty.");
+    }
+  };
 
-    return (
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack
-          spacing={4}
-          w={'full'}
-          maxW={'md'}
-          bg={useColorModeValue('white', 'gray.700')}
-          rounded={'xl'}
-          boxShadow={'lg'}
-          p={6}
-          my={12}>
-          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
-            Enter new password
-          </Heading>
-          <FormControl id="passsword" isRequired>
-            <FormLabel>New Password</FormLabel>
-            <Input
-              type="password"
-              value={resetInput.newPassword}
-              onChange={handleChange}
-              name="newPassword"
-            />
-          </FormControl>
-          <FormControl id="password" isRequired>
-            <FormLabel>Confirm Pssword</FormLabel>
-            <Input 
-                type="password" 
-                value={resetInput.confirmPassword}
-                onChange={handleChange}
-                name="confirmPassword"
-            />
-          </FormControl>
-          <Stack spacing={6}>
-            <Button
-              bg={'blue.400'}
-              color={'white'}
-              _hover={{
-                bg: 'blue.500',
-              }}
-              onClick={handleSubmit}
-              >
-              Submit
-            </Button>
-          </Stack>
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack
+        spacing={4}
+        w={"full"}
+        maxW={"md"}
+        bg={useColorModeValue("white", "gray.700")}
+        rounded={"xl"}
+        boxShadow={"lg"}
+        p={6}
+        my={12}
+      >
+        <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
+          Enter new password
+        </Heading>
+        <FormControl id="passsword" isRequired>
+          <FormLabel>New Password</FormLabel>
+          <Input
+            type="password"
+            value={resetInput.newPassword}
+            onChange={handleChange}
+            name="newPassword"
+          />
+        </FormControl>
+        <FormControl id="password" isRequired>
+          <FormLabel>Confirm Pssword</FormLabel>
+          <Input
+            type="password"
+            value={resetInput.confirmPassword}
+            onChange={handleChange}
+            name="confirmPassword"
+          />
+        </FormControl>
+        <Stack spacing={6}>
+          <Button
+            bg={"blue.400"}
+            color={"white"}
+            _hover={{
+              bg: "blue.500",
+            }}
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
         </Stack>
-      </Flex>
-    );
-  }
+      </Stack>
+    </Flex>
+  );
+};
 
-  export default ResetPassword;
+export default ResetPassword;

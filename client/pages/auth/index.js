4f73fc2b970aa7a822c0cd49accad35722fd5f69
pavanleton <pavankumarkpkp@gmail.com>
2021-12-28 09:28:03 +0530

    refactor: move auth pages to auth directory

---
+++ client/pages/auth/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useRouter } from 'next/router'
+
+export default function AuthHome() {
+    const router = useRouter();
+
+    React.useEffect(() => {
+        router.push("/auth/login");
+    },[]);
+
+  return (
+    <div>
+
+    </div>
+  );
+}

df823c076fd0f3dbbaa1aabe9696311974955b02
pavanleton <pavankumarkpkp@gmail.com>
2021-12-28 09:21:44 +0530

    refactor: remove comments

--- contracts/scripts/deploy-token.js
+++ contracts/scripts/deploy-token.js
@@ -1,19 +1,6 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
   const PRIToken = await hre.ethers.getContractFactory("PRIToken");
   const token = await PRIToken.deploy();
 
@@ -22,8 +9,6 @@ async function main() {
   console.log("PRI token deployed to:", token.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {

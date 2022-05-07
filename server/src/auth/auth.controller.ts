14e78491a8588faf54b96d2e254377726edfc7c6
pavanleton <pavankumarkpkp@gmail.com>
2022-05-07 15:58:07 +0530

    update: add data validation to login controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import {AuthService} from './auth.service'
-import {LoginUserDto} from './interfaces'
+import {LoginUserDto} from './DTO/loginUser'
 
 @Controller('auth')
 export class AuthController {

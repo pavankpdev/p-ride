32f8abcb23d2d9dc147068cb7d62f7556d281cc8
pavanleton <pavankumarkpkp@gmail.com>
2022-06-16 00:26:00 +0530

    update: handle map update in driver details page

--- driver/components/map/index.tsx
+++ driver/components/map/index.tsx
@@ -17,64 +17,54 @@ const Map: React.FC<{isRideStarted: boolean}> = ({isRideStarted}) => {
     const {getRideDetails, currentLocation} = useContext(LocationContext)
 
     React.useEffect(() => {
-        const debounceHandler = setTimeout(() => {
-            loader.load().then(() => {
+        loader.load().then(async () => {
 
-                const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-                    center: {
-                        lat: 12.9736067,
-                        lng: 77.5517457,
-                    },
-                    zoom: 15,
-                });
+            const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+                center: {
+                    lat: 12.9736067,
+                    lng: 77.5517457,
+                },
+                zoom: 15,
+            });
 
-                const directionsService = new google.maps.DirectionsService();
-                const directionsRenderer = new google.maps.DirectionsRenderer();
+            const directionsService = new google.maps.DirectionsService();
+            const directionsRenderer = new google.maps.DirectionsRenderer();
 
-                directionsRenderer.setOptions({
-                    polylineOptions: {
-                        strokeColor: '#000ce6'
-                    }
-                });
-
-                directionsRenderer.setMap(map);
-                if(router.query?.id) {
-                    const ride = getRideDetails(router.query?.id as string);
-
-                    if(!ride) return
-                    let origin;
-                    let destination;
-
-                    if(isRideStarted){
-                        origin = ride?.from?.geometry;
-                        destination = ride?.to?.geometry;
-                    }else {
-                        origin = currentLocation?.geometry
-                        destination = ride?.from?.geometry
-                    }
-
-                    console.log(origin, destination)
-
-                    directionsService
-                        .route({
-                            origin,
-                            destination,
-                            travelMode: google.maps.TravelMode.DRIVING,
-                        })
-                        .then((resp) => directionsRenderer.setDirections(resp))
-                        .catch((err) => console.log(err))
+            directionsRenderer.setOptions({
+                polylineOptions: {
+                    strokeColor: '#000ce6'
                 }
+            });
+
+            directionsRenderer.setMap(map);
+            if(!router?.query?.id) return
+
+            const ride = getRideDetails(router.query?.id as string);
+
+            if(!ride) return
+
+            let origin = currentLocation.geometry
+            let destination = ride?.from?.geometry
+
+            if(isRideStarted){
+                origin = ride?.from?.geometry;
+                destination = ride?.to?.geometry;
+            }
+
+            console.log(origin, destination)
+
+            directionsService
+                .route({
+                    origin,
+                    destination,
+                    travelMode: google.maps.TravelMode.DRIVING,
+                })
+                .then((resp) => directionsRenderer.setDirections(resp))
+                .catch((err) => console.log(err))
 
 
-            })
-        }, 2000);
-
-        return () => {
-            clearTimeout(debounceHandler);
-        };
-
-
-    }, [loader, router, isRideStarted]);
+        })
+    }, [loader, router, isRideStarted, currentLocation]);
 
     return <Box id="map" h={"100%"} w={"100%"} />;
 };
